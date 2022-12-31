62953f3c2678ec0e2ea9a66cee9f6a93e8908b77
Bushra Al-Hetelah <51024435+BushraH2000@users.noreply.github.com>
2022-12-31 22:03:46 +0300

    jj

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
 import subprocess
-os.system('cmd /k "python yolov5/detect.py --weights yolov5/runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source PP.jpg"') 
+os.system('cmd /k "python yolov5/detect.py --weights yolov5/runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source gg.jpg"') 
 #python detect.py --weights runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source ../PP.jpg
 
