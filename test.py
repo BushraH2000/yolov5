fa738d24fc0c1ebf203eb89ed15e3d61a3bbcb48
Bushra Al-Hetelah <51024435+BushraH2000@users.noreply.github.com>
2023-01-06 22:55:13 +0300

    nn

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
-os.system('cmd /k "python yolov5/detect.py --weights yolov5/runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source gg.jpg"') 
+os.system('cmd /k "python B_Armor/yolov5/detect.py --weights yolov5/runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source HH.jpg"') 
+#os.system('cmd /k "python yolov5/detect.py --weights yolov5/runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source gg.jpg"') 
+
+
 #python detect.py --weights runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source ../PP.jpg
 
